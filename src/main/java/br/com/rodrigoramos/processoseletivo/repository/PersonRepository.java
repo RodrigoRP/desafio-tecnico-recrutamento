ac79611eba8817da3af60054192d267b5a0a539e
RodrigoRP <rodrigo.ramos.pa@gmail.com>
2020-01-13 20:39:28 -0300

    "First commit"

--- src/main/java/br/com/rodrigoramos/processoseletivo/repository/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.rodrigoramos.processoseletivo.repository;
-
-import br.com.rodrigoramos.processoseletivo.model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
